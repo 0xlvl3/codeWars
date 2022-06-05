05d99e603cca77c03a639298e985037f926b0330
0xlvl3 <cryptochoccy@gmail.com>
2022-06-05 16:12:37 +1000

    05-06-22 challenges

---
+++ 05-06-22/script.js
@@ -0,0 +1,65 @@
+"use strict";
+
+//1
+
+// Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+
+// Examples:
+// Input: 42145 Output: 54421
+
+// Input: 145263 Output: 654321
+
+// Input: 123456789 Output: 987654321
+
+// function descendingOrder(n) {
+//   return parseInt(String(n).split(``).sort().reverse().join(``));
+// }
+
+// console.log(descendingOrder(1203920));
+
+//2
+
+// Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+// If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+
+// Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+
+// i.e.
+
+// friend ["Ryan", "Kieran", "Mark"] `shouldBe` ["Ryan", "Mark"]
+// Note: keep the original order of the names in the output.
+
+// function friend(friends) {
+//   return friends.filter((n) => n.length === 4);
+// }
+
+//3
+
+// Given an array of ones and zeroes, convert the equivalent binary value to an integer.
+
+// Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
+
+// Examples:
+
+// Testing: [0, 0, 0, 1] ==> 1
+// Testing: [0, 0, 1, 0] ==> 2
+// Testing: [0, 1, 0, 1] ==> 5
+// Testing: [1, 0, 0, 1] ==> 9
+// Testing: [0, 0, 1, 0] ==> 2
+// Testing: [0, 1, 1, 0] ==> 6
+// Testing: [1, 1, 1, 1] ==> 15
+// Testing: [1, 0, 1, 1] ==> 11
+
+//my solution - worked
+const binaryArrayToNumber = (arr) => {
+  // your code
+  const binaryString = arr.join(``);
+  return parseInt(binaryString, 2);
+};
+
+console.log(binaryArrayToNumber([0, 1, 0, 1]));
+
+//other user solution
+
+// const binaryArrayToNumber = arr => parseInt(arr.join(''), 2);
