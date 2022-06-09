bb615f2399418be392db619104bed775875ce29f
0xlvl3 <cryptochoccy@gmail.com>
2022-06-09 16:09:23 +1000

    09-06-22 challenges

---
+++ 09-06-22/script.js
@@ -0,0 +1,70 @@
+"use strict";
+
+//1
+
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+
+// const maps = (x) => {
+//   return x.map((n) => n * 2);
+// };
+
+// const maps = x => x.map(n => n * 2);
+
+//2
+
+// This function should test if the factor is a factor of base.
+
+// Return true if it is a factor or false if it is not.
+
+// About factors
+// Factors are numbers you can multiply together to get another number.
+
+// 2 and 3 are factors of 6 because: 2 * 3 = 6
+
+// You can find a factor by dividing numbers. If the remainder is 0 then the number is a factor.
+// You can use the mod operator (%) in most languages to check for a remainder
+// For example 2 is not a factor of 7 because: 7 % 2 = 1
+
+// Note: base is a non-negative number, factor is a positive number.
+
+// const checkForFactor = (x, y) => x % y === 0 ? true : false;
+
+//3
+// Mr. Scrooge has a sum of money 'P' that he wants to invest. Before he does, he wants to know how many years 'Y' this sum 'P' has to be kept in the bank in order for it to amount to a desired sum of money 'D'.
+
+// The sum is kept for 'Y' years in the bank where interest 'I' is paid yearly. After paying taxes 'T' for the year the new sum is re-invested.
+
+// Note to Tax: not the invested principal is taxed, but only the year's accrued interest
+
+// Example:
+
+//   Let P be the Principal = 1000.00
+//   Let I be the Interest Rate = 0.05
+//   Let T be the Tax Rate = 0.18
+//   Let D be the Desired Sum = 1100.00
+
+// After 1st Year -->
+//   P = 1041.00
+// After 2nd Year -->
+//   P = 1083.86
+// After 3rd Year -->
+//   P = 1128.30
+// Thus Mr. Scrooge has to wait for 3 years for the initial principal to amount to the desired sum.
+
+// Your task is to complete the method provided and return the number of years 'Y' as a whole in order for Mr. Scrooge to get the desired sum.
+
+// Assumption: Assume that Desired Principal 'D' is always greater than the initial principal. However it is best to take into consideration that if Desired Principal 'D' is equal to Principal 'P' this should return 0 Years.
+
+// function calculateYears(principal, interest, tax, desired) {
+//   // your code
+//   let years = 0;
+//   while (principal < desired) {
+//     principal += principal * interest * (1 - tax);
+//     years++;
+//   }
+//   return years;
+// }
