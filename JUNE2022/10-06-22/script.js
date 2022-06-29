6cc683fa27cfeb2b56be9bab34f0f1428ef9aa04
0xlvl3 <cryptochoccy@gmail.com>
2022-06-30 06:04:40 +1000

    Changed layout

---
+++ JUNE2022/10-06-22/script.js
@@ -0,0 +1,101 @@
+"use strict"
+
+//1
+
+// You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+
+// #Examples:
+
+// Kata.getMiddle("test") should return "es"
+
+// Kata.getMiddle("testing") should return "t"
+
+// Kata.getMiddle("middle") should return "dd"
+
+// Kata.getMiddle("A") should return "A"
+// #Input
+
+// A word (string) of length 0 < str < 1000 (In javascript you may get slightly more than 1000 in some test cases due to an error in the test cases). You do not need to test for this. This is only here to tell you that you do not need to worry about your solution timing out.
+
+// #Output
+
+// The middle character(s) of the word represented as a string.
+
+let position;
+let length;
+
+function getMiddle(s) {
+  if (s.length % 2 === 1) {
+    position = s.length / 2;
+    length = 1;
+  } else {
+    position = s.length / 2 - 1;
+    length = 2;
+  }
+  return s.substring(position, position + length);
+}
+
+//2
+
+// Your start-up's BA has told marketing that your website has a large audience in Scandinavia and surrounding countries. Marketing thinks it would be great to welcome visitors to the site in their own language. Luckily you already use an API that detects the user's location, so this is an easy win.
+
+// The Task
+// Think of a way to store the languages as a database (eg an object). The languages are listed below so you can copy and paste!
+// Write a 'welcome' function that takes a parameter 'language' (always a string), and returns a greeting - if you have it in your database. It should default to English if the language is not in the database, or in the event of an invalid input.
+
+const database = {
+  english: 'Welcome',
+  czech: 'Vitejte',
+  danish: 'Velkomst',
+  dutch: 'Welkom',
+  estonian: 'Tere tulemast',
+  finnish: 'Tervetuloa',
+  flemish: 'Welgekomen',
+  french: 'Bienvenue',
+  german: 'Willkommen',
+  irish: 'Failte',
+  italian: 'Benvenuto',
+  latvian: 'Gaidits',
+  lithuanian: 'Laukiamas',
+  polish: 'Witamy',
+  spanish: 'Bienvenido',
+  swedish: 'Valkommen',
+  welsh: 'Croeso'
+}
+
+function greeting(language) {
+  return database[language.toLowerCase()] || "Welcome";
+}
+
+
+//3
+
+// Description:
+// Make a simple function called greet that returns the most-famous "hello world!".
+
+// Style Points
+// Sure, this is about as easy as it gets. But how clever can you be to create the most creative hello world you can think of? What is a "hello world" solution you would want to show your friends?
+
+// const greet = () => `hello world!`
+
+/*
+⢳⣿⣿⡏⢚⠿⠓⡨⠒⠒⠒⠲⡄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+⣾⣿⣿⢣⢿⡕⠫⠊⢀⢌⠹⡐⡟⢣⠄⠄⠄⢀⣠⣤⣤⣤⣤⣤⣀⠄⠄⠄
+⣸⣿⣿⣿⣠⠏⡔⠁⠄⠐⠢⠣⢡⣙⠄⡇⣶⣿⣿⣿⣿⢟⣻⣿⣿⠏⠄⠄
+⣿⣿⣿⡇⢩⠘⣴⣿⣥⣤⢦⢁⠄⠉⡄⡇⠛⠛⠛⢛⣭⣾⣿⣿⡏⠄⠄⠄
+⣿⣿⣿⡇⠹⢇⡹⣿⣿⣛⣓⣿⡿⠞⠑⣱⠄⢀⣴⣿⣿⣿⣿⡟⠄⠄⠄⠄
+⣿⣿⣿⣧⣸⡄⣿⣪⡻⣿⠿⠋⠄⠄⣀⣀⢡⣿⣿⣿⣿⡿⠋⠄⠄⠄⠄⠄
+⠘⣿⣿⣿⣿⣷⣭⣓⡽⡆⡄⢀⣤⣾⣿⣿⣿⣿⣿⡿⠋⠄⠄⠄⠄⠄⠄⠄
+⠄⢨⡻⡇⣿⢿⣿⣿⣭⡶⣿⣿⣿⣜⢿⡇⡿⠟⠉⠄⠄⠄⠄⠄⠄⠄⠄⠄
+⠄⠸⣷⡅⣫⣾⣿⣿⣿⣷⣙⢿⣿⣿⣷⣦⣚⡀⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+⠄⠄⢉⣾⡟ */ function greet() {
+  return "hello world!"
+}/*⠄
+⠄⠄⠻⢿⣿⣿⣿⣿⣿⣿⣿⢟⣫⣾⣿⣷⡹⣿⣿⣿⣿⣿⣿⣿⡟⠄⠄⠄
+⠄⠄⠄⠄⢮⣭⣍⡭⣭⡵⣾⣿⣿⣿⡎⣿⣿⣌⠻⠿⠿⠿⠟⠋⠄⠄⠄⠄
+⠄⠄⠄⠄⠈⠻⣿⣿⣿⣿⣹⣿⣿⣿⡇⣿⣿⡿⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+⠄⠄⣀⣴⣾⣶⡞⣿⣿⣿⣿⣿⣿⣿⣾⣿⡿⠃⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+⣠⣾⣿⣿⣿⣿⣿⣹⣿⣿⣿⣿⣿⡟⣹⣿⣳⡄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+*/
+
+console.log(greet())
