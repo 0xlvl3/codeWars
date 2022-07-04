6a1dfb2a67ae2625104621cfb1ad832eefae23c6
0xlvl3 <cryptochoccy@gmail.com>
2022-07-04 14:23:14 +1000

    04-07-22 Challenges complete

---
+++ JULY2022/04-07-22/script.js
@@ -0,0 +1,49 @@
+"use strict";
+
+//1
+
+// You live in the city of Cartesia where all roads are laid out in a perfect grid. You arrived ten minutes too early to an appointment, so you decided to take the opportunity to go for a short walk. The city provides its citizens with a Walk Generating App on their phones -- everytime you press the button it sends you an array of one-letter strings representing directions to walk (eg. ['n', 's', 'w', 'e']). You always walk only a single block for each letter (direction) and you know it takes you one minute to traverse one city block, so create a function that will return true if the walk the app gives you will take you exactly ten minutes (you don't want to be early or late!) and will, of course, return you to your starting point. Return false otherwise.
+
+// Note: you will always receive a valid array containing a random assortment of direction letters ('n', 's', 'e', or 'w' only). It will never give you an empty array (that's not a walk, that's standing still!).
+
+//my solution
+// function isValidWalk(walk) {
+//   //insert brilliant code here
+//   if (walk.length === 10 && walk.slice(0, 5) === walk.slice(5, 10)) {
+//     return true;
+//   } else false;
+// }
+
+//other users
+function isValidWalk(walk) {
+  function count(val) {
+    return walk.filter(function (a) {
+      return a == val;
+    }).length;
+  }
+  return (
+    walk.length == 10 && count("n") == count("s") && count("w") == count("e")
+  );
+}
+
+//2
+
+function nameSuffle(str) {
+  return str.split(" ").reverse().join(" ");
+}
+
+//3
+
+// This time no story, no theory. The examples below show you how to write function accum:
+
+// Examples:
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+
+function accum(s) {
+  return s
+    .split("")
+    .map((c, i) => c.toUpperCase() + c.toLowerCase().repeat(i))
+    .join("-");
+}
